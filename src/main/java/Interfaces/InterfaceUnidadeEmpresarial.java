1a3d77ff75a7386ab5c86a47ac86b785c1951572
JOAO GABRIEL MAURICIO <joao.gmauricio@senacsp.edu.br>
2019-10-17 22:08:26 -0300

    up

---
+++ src/main/java/Interfaces/InterfaceUnidadeEmpresarial.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaces;
+
+import Dominios.UnidadeEmpresarial;
+
+public interface InterfaceUnidadeEmpresarial extends InterfaceBase<UnidadeEmpresarial> {
+    
+}
---
+++ src/main/java/Repositorios/RepositorioUnidadeEmpresarial.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Repositorios;
+
+import Dominios.UnidadeEmpresarial;
+import Interfaces.InterfaceUnidadeEmpresarial;
+import java.util.List;
+
+/**
+ *
+ * @author joao.gmauricio
+ */
+public class RepositorioUnidadeEmpresarial implements InterfaceUnidadeEmpresarial{
+
+    public void Add(UnidadeEmpresarial obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public UnidadeEmpresarial GetById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<UnidadeEmpresarial> GetAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void Update(UnidadeEmpresarial obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void Remove(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
